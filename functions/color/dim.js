986e238d4d2d1e519d44bf8893e034423594d921
Will Tesler <willtesler@gmail.com>
2022-07-18 20:02:41 -0500

    Delete unused color functions

--- functions/color/dim.js
+++
@@ -1,5 +0,0 @@
-module.exports = function(rgbObj, t) {
-  for (const key of Object.keys(rgbObj)) {
-    rgbObj[key] *= t;
-  }
-};
--- functions/lerp/lerpColor2.js
+++
@@ -1,9 +0,0 @@
-const lerp = require('./lerp');
-
-module.exports = function (a, b, t) {
-  return {
-    r: lerp(a.r, b.r, t),
-    g: lerp(a.g, b.g, t),
-    b: lerp(a.b, b.b, t)
-  };
-};
